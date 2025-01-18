5bd5b6a0049f80de0573be5c2f2aa571827ad330
Ramazan <zanadat4@gmail.com>
2025-01-18 17:42:02 +0500

    Revert "Task 1"

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,19 +1,3 @@
 import { Routes } from '@angular/router';
-import { UsersListComponent } from './users-list/users-list.component';
-import { MainComponent } from './main/main.component';
-import { UsersApiService } from './users-api.service';
 
-export const routes: Routes = [
-    {
-        path: 'users',
-        component: UsersListComponent
-    },
-    {
-        path: '',
-        component: MainComponent
-    },
-    {
-        path: 'todos',
-        component: UsersApiService
-    }
-];
+export const routes: Routes = [];
